f8d73a5386bfa36cf3dcc3ef413daa4b21ec773d
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-12-11 15:56:20 -0300

    feat: add InputLabel component

---
+++ src/components/ui/input-label.tsx
@@ -0,0 +1,22 @@
+import { twMerge } from "tailwind-merge";
+import { Input, InputProps } from "./input";
+
+interface InputLabelProps extends InputProps {
+  label: string;
+}
+
+const InputLabel = ({ className, label, ...props }: InputLabelProps) => {
+  return (
+    <label className="flex flex-col gap-2">
+      <span className="text-clamp3Xl capitalize">{label}</span>
+      <Input
+        className={`bg-input placeholder:text-inputText text-clampXl sm:py-7 sm:px-4 ${twMerge(
+          className
+        )}`}
+        {...props}
+      />
+    </label>
+  );
+};
+
+export default InputLabel;
