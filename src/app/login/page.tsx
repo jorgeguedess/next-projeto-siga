30d04b9e11f36857772adca85fc2da11bc23b798
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-12-13 00:17:56 -0300

    fix: add new logo src for Login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,16 +3,16 @@ import Image from "next/image";
 
 export default function Login() {
   return (
-    <main className="min-h-screen w-full flex flex-col  justify-center items-center py-2 px-6">
+    <main className="flex min-h-screen w-full flex-col  items-center justify-center px-6 py-2">
       <div className="w-clampForm">
         <Image
-          src="/logo.png"
+          src="/logo-black.svg"
           alt="SIGA - SISTEMA INTEGRADO DE GESTÃO ACADEMICA"
           width={0}
           height={0}
           sizes="100vw"
           style={{ objectFit: "cover" }}
-          className="w-full sm:mb-10 mb-5"
+          className="mb-5 w-full sm:mb-10"
         />
         <LoginForm />
       </div>
