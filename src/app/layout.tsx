76f24b6863eb6b2b7373825d7b0ec5fd2352518e
Jorge Guedes <jorge_guedes00@hotmail.com>
2023-12-14 00:06:36 -0300

    styles: remove padding in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
         className={`${roboto.variable} ${lato.variable} flex min-h-screen flex-col font-main`}
       >
         <Header />
-        <div className="flex-1 px-5 py-10">{children}</div>
+        <div className="flex-1 py-10">{children}</div>
       </body>
     </html>
   );
